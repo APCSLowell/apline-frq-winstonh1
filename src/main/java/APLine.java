852b9dc3a885ba3650d9e1feadc25f3b8bc2f7d1
winstonh1 <wihuang2@s.sfusd.edu>
2025-04-21 09:57:04 -0700

    Update APLine.java

--- src/main/java/APLine.java
+++ src/main/java/APLine.java
@@ -1,5 +1,19 @@
 public class APLine
 {
-  /* your code here */
-  
+  private int mya,myb,myc;
+  public APLine(int a, int b, int c){
+    mya = a;
+    myb = b;
+    myc = c;
+  }
+  public doube getSlope(){
+    double slope = (double) -mya/myb;
+    return slope;
+  }
+  public boolean isOnLine(int x, int y){
+    if(((mya * x) +(myb * y) + c) == 0){
+      return true;
+    }
+    return false;
+  }
 }
